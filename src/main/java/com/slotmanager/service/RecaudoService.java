98150b2bf2f3439f12581fcc8fd8109113fb1132
torvic886 <vagutierrezv@uqvirtual.edu.co>
2025-03-21 11:56:02 -0500

    Se comentan librerias no usadas

--- src/main/java/com/slotmanager/service/RecaudoService.java
+++ src/main/java/com/slotmanager/service/RecaudoService.java
@@ -9,7 +9,7 @@ import com.slotmanager.repository.RecaudoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.Date;
+//import java.util.Date;
 import java.util.List;
 
 @Service
